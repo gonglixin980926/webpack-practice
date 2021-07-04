f621286538782b2a47f875877d5422563e43e001
gonglixin <gonglixin@yuanfudao.com>
2021-07-04 16:27:31 +0800

    study: promise study

--- src/promiseImplement/simpleDemo.ts
+++ src/promiseImplement/simpleDemo.ts
@@ -1,33 +1,46 @@
+export enum MyPromiseState{
+    PENDING = 'pending',
+    FULFILLED = 'filfilled',
+    REJECT = 'reject'
+}
+
+// 极简实现 + 链式调用 + 状态
 class MyPromise{
     callbacks = []
+    state = MyPromiseState.PENDING
+    value = null
     constructor(fn){
         fn(this._resolve.bind(this))
     }
 
     then(onFulfilled){
-        console.log(this,"then")
-        this.callbacks.push(onFulfilled)
-        return this
+        if(this.state === MyPromiseState.PENDING){
+            this.callbacks.push(onFulfilled)
+        }else{
+            onFulfilled(this.value)
+        }
+        return this // 实现promise链式调用
     }
 
     _resolve(value){
+        this.state = MyPromiseState.FULFILLED
+        this.value = value
         this.callbacks.forEach( onFulfilled => onFulfilled(value))
     }
 }
 
 function myPromiseTest() {
     //Promise应用
-    const a = new MyPromise(resolve => {
-        setTimeout(() => {
-            console.log('done');
-            resolve('5秒');
-        }, 5000);
-        console.error("resolve")
-    }).then((tip) => {
-        console.log(this)
+    const proA = new MyPromise(resolve => {
+            setTimeout(()=>{
+                resolve('5秒');
+            },5000)
+    })
+    const proB = proA.then((tip) => {
         console.log('第一次promise',tip);
-    }).then(res=>{
-        console.log(`第二次promise ${res}`)
+    })
+    proB.then((res)=>{
+        console.log(`第二次promise`, res)
     })
 }
 myPromiseTest()
